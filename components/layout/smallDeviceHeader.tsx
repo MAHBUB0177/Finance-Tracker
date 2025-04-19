64843a40a411d48e0ae8e18b4de2f930c3a39535
@mahbub.alam <mahbub.alam@technonext.com>
2025-04-19 23:48:29 +0600

    mobile device navbar fixed

--- components/layout/smallDeviceHeader.tsx
+++ components/layout/smallDeviceHeader.tsx
@@ -20,48 +20,32 @@ const SmallDeviceHeader = ({ isOpen, setIsOpen }: SidebarProps) => {
         <Link href={'/'}>
           <div className='flex gap-1'>
             <div className=''>
-            <FiMenu
-              className="h-6 w-6 cursor-pointer "
-              onClick={() => setIsOpen((prev) => !prev)}
-            />
+              <FiMenu
+                className="h-6 w-6 cursor-pointer "
+                onClick={() => setIsOpen((prev) => !prev)}
+              />
             </div>
-           
+
 
             <p className='text-xl text-red-500 font-bold pt-1'>TrendyCart</p>
           </div>
         </Link>
 
         <div className='flex justify-start gap-7 pt-1'>
-          {/* {sessionStatus == 'authenticated' ? (
-            <>
-              <p className="flex justify-center items-center cursor-pointer text-normal h-[35px] w-[35px] font-semibold rounded-full bg-secondary text-white pt-2"
-                onClick={() => setShow(!show)}>
-                {authUserData?.name?.charAt(0).toUpperCase()}
-              </p>{" "}
-            </>
-          ) : ( */}
           <div className='flex justify-between gap-2'>
-            <Link href={'/auth'} >
-              <button
-                className={"w-20 text-sm p-[6px] font-semibold border hover:bg-red-200 border-red-500  text-secondary rounded-md hover:scale-105 duration-300"}
-              >
-                Sign in
 
-              </button>
-            </Link>
+            <button
+              className={"w-20 cursor-pointer text-sm p-[6px] font-semibold bg-red-500 rounded-sm text-white"}
+            >
+              LogIn
+            </button>
 
-            <Link href={'/signup'} >
-              <button
 
-                className={"w-20 text-sm p-[6px] font-semibold bg-red-500 rounded-sm text-white"}
-              >
-                Sign up
-              </button>
-            </Link>
+
           </div>
 
 
-          {/* )} */}
+
 
         </div>
 
